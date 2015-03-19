4b410c64e76584bb138381fd3a2452bc577453aa
Sam Rang <srang2010@gmail.com>
2015-03-18 19:23:55 -0500

    less compilation, js minification and unit tests, oh yeah and grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,95 @@
-//Gruntfile
 module.exports = function(grunt) {
 
   //Initializing the configuration object
   grunt.initConfig({
 
-      // Task configuration
+      //--------------------CSS-GENERATION---------------------//
+      less: { 
+        development: {
+          options: {
+            compress: true,  //minifying the result
+          },
+          files: {
+            //compiling frontend.less into frontend.css
+            "./css/frontend.css":"./less/frontend.less",
+            //compiling backend.less into backend.css
+            "./css/backend.css":"./less/backend.less"
+          }
+        }
+      },
+      //--------------------MINIFICATION---------------------//
       concat: {
-        //...
+        options: {
+          separator: ';',
+        },
+        js_frontend: {
+          src: [
+            './bower_components/jquery/jquery.js',
+            './bower_components/bootstrap/dist/js/bootstrap.js',
+            './js/frontend.js'
+        ],
+        dest: './js/frontend.min.js',
+        },
+        js_backend: {
+          src: [
+            './bower_components/jquery/jquery.js',
+            './bower_components/bootstrap/dist/js/bootstrap.js',
+            './app/assets/javascript/backend.js'
+        ],
+        dest: './js/backend.min.js',
+        },
       },
-      less{
-        //...
+      uglify: {
+        options: {
+          mangle: false  // Use if you want the names of your functions and variables unchanged
+        },
+        frontend: {
+          files: {
+            './js/frontend.min.js': './js/frontend.min.js',
+          }
+        },
+        backend: {
+          files: {
+            './js/backend.min.js': './js/backend.min.js',
+          }
+        },
       },
-      uglify{
-        //...
+      //--------------------TESTING---------------------//
+      phpunit: {
+        classes: {
+        },
+        options: {
+          colors: true
+        }
       },
-      phpunit{
-        //...
+      qunit: {
+        files: ['test/*.html']
       },
-      watch{
-        //...
+      bootlint: {
+        options: {
+          stoponerror: false,
+          relaxerror: []
+        },
+        files: ['', '']
+      },
+      curl: {
+        'test/target/index.php' : 'http://localhost/tourney/'
       }
   });
 
   // Plugin loading
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-bootlint');
+  grunt.loadNpmTasks('grunt-phplint');
+  grunt.loadNpmTasks('grunt-phpunit');
+  grunt.loadNpmTasks('grunt-curl');
 
   // Task definition
+  grunt.registerTask('default', ['']);
+  grunt.registerTask('lint', ['curl','jshint','bootlint','phplint','clean']);
+  grunt.registerTask('minify', ['less', 'concat', 'uglify']);
+  grunt.registerTask('qunit', ['qunit']);
 
-}
+};
---
+++ test/test.js
@@ -0,0 +1,4 @@
+module('Hello Testing');
+test('Hello', function(assert) {
+  assert.ok( 1 == "1", "Passed!");
+});
