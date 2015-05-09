7412aff3b62ae61d887e3baf3ed7a47d57f198bf
Sam Rang <srang2010@gmail.com>
2015-05-09 12:03:29 -0500

    updated form

--- fragments/edit_user.php
+++ fragments/edit_user.php
@@ -19,66 +19,47 @@
         </div>
       </div>
     </div>
-    <div class='form-group'>
-      <label for='inputEmail' class='col-lg-1 control-label'>Email</label>
-      <div class='col-lg-11'>
-        <input class='form-control' id='inputEmail' placeholder='Email' type='email'>
-      </div>
-    </div>
-    <div class='form-group'>
-      <label for='inputPassword' class='col-lg-1 control-label'>Password</label>
-      <div class='col-lg-11'>
-        <input class='form-control' id='inputPassword' placeholder='Password' type='password' name='password'>
-      </div>
-    </div>
-    <div class='form-group'>
-      <label for='confirmPassword' class='col-lg-1 control-label'>Confirm</label>
-      <div class='col-lg-11'>
-        <input class='form-control' id='confirmPassword' placeholder='Retype Password' type='password' name='confirm'>
-      </div>
-    </div>
-    <div class='form-group'>
-      <label for='textArea' class='col-lg-2 control-label'>Textarea</label>
-      <div class='col-lg-10'>
-        <textarea class='form-control' rows='3' id='textArea'></textarea>
-        <span class='help-block'>A longer block of help text that breaks onto a new line and may extend beyond one line.</span>
-      </div>
-    </div>
-    <div class='form-group'>
-      <label class='col-lg-2 control-label'>Radios</label>
-      <div class='col-lg-10'>
-        <div class='radio'>
-          <label>
-            <input name='optionsRadios' id='optionsRadios1' value='option1' checked='' type='radio'>
-            Option one is this
-          </label>
+    <div class='row'>
+      <div class='col-lg-6'>
+        <div class='form-group'>
+          <label for='inputEmail' class='col-lg-2 control-label'>Email</label>
+          <div class='col-lg-10'>
+            <input class='form-control' id='inputEmail' placeholder='Email' type='email'>
+          </div>
         </div>
-        <div class='radio'>
-          <label>
-            <input name='optionsRadios' id='optionsRadios2' value='option2' type='radio'>
-            Option two can be something else
-          </label>
+      </div>
+      <div class='col-lg-6'>
+        <div class='form-group'>
+          <label for='inputPhone' class='col-lg-2 control-label'>Phone</label>
+          <div class='col-lg-10'>
+            <input id='inputPhone' class='form-control' placeholder='Phone Number' type='tel'>
+          </div>
         </div>
       </div>
     </div>
+    <div class='row'>
+      <div class='col-lg-6'>
+        <div class='form-group'>
+          <label for='inputPassword' class='col-lg-2 control-label'>Password</label>
+          <div class='col-lg-10'>
+            <input id='inputPassword' class='form-control' placeholder='Password' type='password' name='password'>
+          </div>
+        </div>
+      </div>
+      <div class='col-lg-6'>
+        <div class='form-group'>
+          <label for='confirmPassword' class='col-lg-2 control-label'>Confirm</label>
+          <div class='col-lg-10'>
+            <input class='form-control' id='confirmPassword' placeholder='Retype Password' type='password' name='confirm'>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr />
     <div class='form-group'>
-      <label for='select' class='col-lg-2 control-label'>Selects</label>
-      <div class='col-lg-10'>
-        <select class='form-control' id='select'>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
-        <br>
-        <select multiple='' class='form-control'>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
+      <label for='inputScreenName' class='col-lg-1 control-label'>Screen Name</label>
+      <div class='col-lg-11'>
+        <input class='form-control' id='inputScreenName' placeholder='Screen Name' type='text' name='screenname'>
       </div>
     </div>
     <div class='form-group'>
