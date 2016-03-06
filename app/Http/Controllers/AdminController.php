19744c49cbff5088956d903ee964f374a36f62e2
Sam Rang <srang2010@gmail.com>
2016-03-06 08:56:42 -0600

    testing

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -416,29 +416,29 @@ class AdminController extends Controller
         #    return redirect('admin/brackets/'.$bracktet->bracket_id)->withInput()->withErrors($errors);
         #}
         // create user bracket
-        DB::beginTransaction();
+        #DB::beginTransaction();
 
-        $bracket_updated = BracketFactory::createBracket($request, new CreateBaseBracketStrategy($this->teamRepo));
+        #$bracket_updated = BracketFactory::createBracket($request, new CreateBaseBracketStrategy($this->teamRepo));
 
-        if (isset($bracket_updated)) {
-            $bracket_updated->name = $request->name;
-            $bracket_updated->user_id = $request->user_id;
-            $bracket_updated->save();
-            // update just creates new and deletes old
-            // consider revisiting this later
-            $bracket->delete();
-            DB::commit();
-            $alert = [
-                'message' => 'Save Successful.',
-                'level' => 'success'
-            ];
-        } else {
-            DB::rollBack();
-            $alert = [
-                'message' => 'Save unsuccessful',
-                'level' => 'danger'
-            ];
-        }
+        #if (isset($bracket_updated)) {
+        #    $bracket_updated->name = $request->name;
+        #    $bracket_updated->user_id = $request->user_id;
+        #    $bracket_updated->save();
+        #    // update just creates new and deletes old
+        #    // consider revisiting this later
+        #    $bracket->delete();
+        #    DB::commit();
+        #    $alert = [
+        #        'message' => 'Save Successful.',
+        #        'level' => 'success'
+        #    ];
+        #} else {
+        #    DB::rollBack();
+        #    $alert = [
+        #        'message' => 'Save unsuccessful',
+        #        'level' => 'danger'
+        #    ];
+        #}
 
         $request->session()->put('alert', $alert);
         return redirect('admin/brackets');
