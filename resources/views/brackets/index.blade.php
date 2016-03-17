75be3aa5e4a55a3e1e590e1f2533a1c528f56dc2
Sam Rang <srang2010@gmail.com>
2016-03-17 00:46:53 -0500

    only roll out to admin now

--- resources/views/brackets/index.blade.php
+++ resources/views/brackets/index.blade.php
@@ -76,9 +76,11 @@
                                             <a class="btn btn-info" href="{{ url('brackets/'.$bracket->bracket_id) }}">
                                                 <i class="fa fa-btn fa-pencil"></i> Edit
                                             </a>
+                                            {{--
                                             <a class="btn btn-primary" href="{{ url('brackets/'.$bracket->bracket_id.'/print') }}" target="_blank">
                                                 <i class="fa fa-btn fa-print"></i> Print
                                             </a>
+                                            --}}
                                         </form>
                                     </div>
                                 </td>
